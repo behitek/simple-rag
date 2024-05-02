55a5dee934959a6a281e30f6b30c7c92fae2f0a4
hieunv <hieunv.dev@gmail.com>
2024-05-02 23:35:57 +0700

    update examples

--- examples/simple_rag_bm25_ollama.py
+++ examples/simple_rag_bm25_ollama.py
@@ -1,13 +1,10 @@
 import os
-import sys
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
-
-from rag.retrieval import BM25Retrieval
-from rag.llm import OllamaLLM
-from rag.rerank import CrossEncoderRerank
-from rag.pipeline import SimpleRAGPipeline, Answer
 from rag.data_helper import PDFReader
+from rag.llm import OllamaLLM
+from rag.pipeline import Answer, SimpleRAGPipeline
+from rag.rerank import CrossEncoderRerank
+from rag.retrieval import BM25Retrieval
 from rag.text_utils import text2chunk
 
 sample_pdf = os.path.join(os.path.dirname(__file__), "sample.pdf")
@@ -21,6 +18,19 @@ llm = OllamaLLM(model_name="llama3:instruct")
 rerank = CrossEncoderRerank(model_name="cross-encoder/ms-marco-MiniLM-L-6-v2")
 pipeline = SimpleRAGPipeline(retrieval=retrieval, llm=llm, rerank=rerank)
 
-query = "What can Ollama do?"
-response: Answer = pipeline.run(query)
-print(response.answer)
+
+def run(query: str) -> Answer:
+    return pipeline.run(query)
+
+
+if __name__ == "__main__":
+    query = "What can Ollama do?"
+    print("Sample query:", query)
+    response: Answer = pipeline.run(query)
+    print(response.answer)
+    print("Now, please ask your own questions!")
+    while True:
+        query = input("Your question: ")
+        response: Answer = run(query)
+        print(response.answer)
+        print()
